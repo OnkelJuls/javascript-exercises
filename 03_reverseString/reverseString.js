caa292e58495c4f2d4bfc53497ed4e0cf5cca7d9
Julian <62448764+OnkelJuls@users.noreply.github.com>
2024-09-10 14:03:25 +0200

    finish no 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  string = str.split("");
+  string.reverse();
+  console.log(string.join(""));
+  return string.join("");
 };
-
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
