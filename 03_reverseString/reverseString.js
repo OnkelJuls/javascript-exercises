db3ac63f9301d62030ca4936757aef0910b59369
OnkelJuls <62448764+OnkelJuls@users.noreply.github.com>
2025-02-10 10:17:02 +0100

    finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = []
+    reverse = string.split('');
+    reversed = reverse.reverse();
+    reversed = reverse.join('');
+    return reversed;
 };
 
 // Do not edit below this line
