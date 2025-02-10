913fe2a0f7097f97de906075f995c5bf8c230b1a
OnkelJuls <62448764+OnkelJuls@users.noreply.github.com>
2025-02-10 11:13:36 +0100

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a ,b) {
+    let sum = 0;
+    let small = 0;
+    let large = 0;
+    if (a < 0 || b < 0){
+        return 'ERROR'
+    }else if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR'
+    }else if (a > b){
+        large = a;
+        small = b;
+    }else if (b > a){
+        small = a;
+        large = b;
+    }
 
+    for (let i = small; i <= large; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
