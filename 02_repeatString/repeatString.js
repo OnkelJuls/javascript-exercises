20313a54d3eee62d9e698159cb393ac8c56da25b
OnkelJuls <62448764+OnkelJuls@users.noreply.github.com>
2025-02-10 10:06:50 +0100

    finish 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    let text = '';
+    if(amount < 0){
+        return 'ERROR';
+    }
+    for(let i = 0; i < amount; i++){
+        text += string;
+    }
+    return text;
 };
 
 // Do not edit below this line
